ed1ccf42068090f22b87788bbd03885e2530c9df
Aaron Lima <aarolima@amazon.com>
2018-10-12 10:58:36 -0400

    Added 3AZ support to AD Sites modified to one site per region

--- scripts/Configure-Sites.ps1
+++ scripts/Configure-Sites.ps1
@@ -11,9 +11,15 @@ param(
 
     [Parameter(Mandatory=$true)]
     [string]$PrivateSubnet2CIDR,
+    
+    [Parameter(Mandatory=$false)]
+    [string]$PublicSubnet3CIDR,
+
+    [Parameter(Mandatory=$false)]
+    [string]$PrivateSubnet3CIDR,
 
     [Parameter(Mandatory=$true)]
-    [string]$Server   
+    [string]$Region   
 )
 
 
@@ -31,13 +37,25 @@ try {
                 $ErrorActionPreference = "Stop"
                 Start-Transcript -Path C:\cfn\log\$($MyInvocation.MyCommand.Name).log -Append
 
-                Get-ADObject -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -filter {Name -eq 'Default-First-Site-Name'} | Rename-ADObject -NewName AZ1
-                New-ADReplicationSite AZ2 -Server $Server
-                New-ADReplicationSubnet -Name $PublicSubnet1CIDR -Site AZ1 -Server $Server
-                New-ADReplicationSubnet -Name $PublicSubnet2CIDR -Site AZ2
-                New-ADReplicationSubnet -Name $PrivateSubnet1CIDR -Site AZ1
-                New-ADReplicationSubnet -Name $PrivateSubnet2CIDR -Site AZ2
-                Get-ADReplicationSiteLink -Filter * | Set-ADReplicationSiteLink -SitesIncluded @{add='AZ2'} -ReplicationFrequencyInMinutes 15
+                Get-ADObject -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -filter {Name -eq 'Default-First-Site-Name'} | Rename-ADObject -NewName $Region
+                New-ADReplicationSubnet -Name $PublicSubnet1CIDR -Site $Region
+                New-ADReplicationSubnet -Name $PublicSubnet2CIDR -Site $Region
+                New-ADReplicationSubnet -Name $PrivateSubnet1CIDR -Site $Region
+                New-ADReplicationSubnet -Name $PrivateSubnet2CIDR -Site $Region
+
+                # AZ3 scenarios only; add 3rd AZ subnets to site1
+                if (!$PrivateSubnet3CIDR ) {
+                    echo "No 3rd Private AZ"
+                } else {
+                    New-ADReplicationSubnet -Name $PrivateSubnet3CIDR -Site $Region
+                }
+
+                if (!$PrivateSubnet3CIDR ) {
+                    echo "No 3rd Private AZ"
+                } else {
+                    New-ADReplicationSubnet -Name $PublicSubnet3CIDR -Site $Region
+                }
+
                 echo "Successfully Configured the AD Sites..."
                 break
             }           
